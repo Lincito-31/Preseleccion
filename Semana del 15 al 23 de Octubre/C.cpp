629716db685fde33e3f56fa49c25e5fedf2d1f48
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-10-19 18:42:09 -0400

    a

---
+++ Semana del 15 al 23 de Octubre/C.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/contest/1733/problem/D2
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define ALL(x) x.begin(),x.end()
+using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int,int> pii;
+typedef tree<pii,null_type,less<pii>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+typedef vector<int> vi;
+typedef long long ll;
+ll t,n,x,y;
+int main(){
+    scanf("%lld",&t);
+    while(t--){
+        scanf("%lld %lld %lld",&n,&x,&y);
+    }
+}
