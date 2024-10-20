766c45fe1190bfbc98b93f04a0273ce346f2c7e3
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-10-20 11:51:14 -0400

    a

--- Semana del 15 al 23 de Octubre/C.cpp
+++ Semana del 15 al 23 de Octubre/C.cpp
@@ -9,9 +9,39 @@ typedef tree<pii,null_type,less<pii>,rb_tree_tag,tree_order_statistics_node_upda
 typedef vector<int> vi;
 typedef long long ll;
 ll t,n,x,y;
+string a,b;
 int main(){
-    scanf("%lld",&t);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cin >> t;
     while(t--){
-        scanf("%lld %lld %lld",&n,&x,&y);
+        cin >> n >> x >> y;
+        cin >> a >> b;
+        ll res=0;
+        vector<ll> err;
+        for(int i=0;i<n;i++){
+            if(a[i]!=b[i]){
+                err.push_back(i);
+            }
+        }
+        n=err.size();
+        if(n%2==1){
+            cout << "-1\n";
+        }else{
+            if(n!=0){
+                if(y<x){
+                    res=(n/2)*y;
+                    if(n==2 && err[1]==err[0]+1){
+                        res=min(2*y,x);
+                    }
+                }else{
+                    vector<ll> dif(n);
+                    for(int i=1;i<n;i++){
+                        dif[i]=err[i]-err[i-1];
+                    }
+                }
+            }
+            cout << res << "\n";
+        }
     }
 }
