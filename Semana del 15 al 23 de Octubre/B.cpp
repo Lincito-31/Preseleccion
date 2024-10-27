41e3d643ee193853a55f18f8f1f86d88ea6328b5
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-10-27 19:00:26 -0400

    a

--- Semana del 15 al 23 de Octubre/B.cpp
+++ Semana del 15 al 23 de Octubre/B.cpp
@@ -9,47 +9,77 @@ typedef long long ll;
 typedef tree<pii,null_type,less<pii>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-ll n,a,t;
+int n,a,t;
 int main(){
-    scanf("%lld",&n);
-    vector<vll> nums(n,vll(3));
-    vll res(n);
-    for(ll i=0;i<n;i++){
-        scanf("%lld %lld",&a,&t);
-        t+=a;
-        nums[i]={a,t,i};
+    scanf("%d",&n);
+    vector<pair<pair<int,int>,int>> nums(n);
+    vi res(n,0);
+    for(int i=0;i<n;i++){
+        scanf("%d%d",&nums[i].first.first,&nums[i].first.second);
+        nums[i].first.second+=nums[i].first.first;
+        nums[i].first.second*=-1;
+        nums[i].second=i;
     }
     sort(ALL(nums));
-    for(ll i=1;i<n;i++){
+    /*for(int i=1;i<n;i++){
         if(nums[i][0]==nums[i-1][0]){  
-            ll temp=nums[i][1];
-            ll temp2=nums[i][2];
+            int temp=nums[i][1];
+            int temp2=nums[i][2];
             nums[i][1]=nums[i-1][1];
             nums[i][2]=nums[i-1][2];
             nums[i-1][1]=temp;
             nums[i-1][2]=temp2;
         }
+    }*/
+    set<pair<int,int>> gua;
+    gua.insert({2e9,0});
+    for(int i=0;i<n;i++){
+        int b=nums[i].second;
+        int c=-nums[i].first.second;
+        auto q=gua.lower_bound({c,-2e9});
+        res[b]=-((*q).second)+1;
+        if((*q).first==c){
+            gua.erase(q);
+        }
+        gua.insert({c,-res[b]});
+        auto p=gua.lower_bound({c,res[b]});
+        while(p!=gua.begin()){
+            p--;
+            if((*p).first<c && -(*p).second<=res[b]){
+                gua.erase(p);
+            }
+        }
     }
-    vector<vector<pair<ll,ll>>> cant(n,vector<pair<ll,ll>>(1,{0,-1}));
-    cant[0].push_back({nums[0][1],nums[0][2]});
-    for(ll i=1;i<n;i++){
-        ll limi=0,limd=n-1;
+    /*vector<int> cant(n+5,-1);
+    cant[0]=nums[0][1];
+    for(int i=1;i<n;i++){
+        int limi=0,limd=n-1;
         while(limi!=limd){
-            ll mid=(limi+limd)/2;
-            if(nums[i][1]<=cant[mid].back().first){
+            int mid=(limi+limd)/2;
+            if(nums[i][1]<=cant[mid]){
                 limi=mid+1;
             }else{
                 limd=mid;
             }
         }
-        cant[limi].push_back({nums[i][1],nums[i][2]});
-    }
-    for(ll i=0;i<n;i++){
-        for(ll j=1;j<cant[i].size();j++){
-            res[cant[i][j].second]=i;
+        while(limi>0){
+            if(nums[i][1]>cant[limi]){
+                limi--;
+            }else{
+                break;
+            }
         }
-    }
-    for(ll i=0;i<n;i++){
-        printf("%lld ",res[i]);
+        while(limi<n-1){
+            if(nums[i][1]<=cant[limi]){
+                limi++;
+            }else{
+                break;
+            }
+        }
+        cant[limi]=nums[i][1];
+        res[nums[i][2]]=limi;
+    }*/
+    for(auto u:res){
+        printf("%d ",u-1);
     }
 }
