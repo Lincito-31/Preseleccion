a8aecc64913c7bacf1da6bb7c5833fe990088995
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-10-28 15:00:42 -0400

    a

--- Semana del 15 al 23 de Octubre/B.cpp
+++ Semana del 15 al 23 de Octubre/B.cpp
@@ -1,12 +1,9 @@
 // https://codeforces.com/gym/104875/attachments  problem J
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 #define ALL(x) x.begin(),x.end()
-using namespace __gnu_pbds;
 using namespace std;
 typedef pair<int,int> pii;
 typedef long long ll;
-typedef tree<pii,null_type,less<pii>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 int n,a,t;
@@ -21,65 +18,22 @@ int main(){
         nums[i].second=i;
     }
     sort(ALL(nums));
-    /*for(int i=1;i<n;i++){
-        if(nums[i][0]==nums[i-1][0]){  
-            int temp=nums[i][1];
-            int temp2=nums[i][2];
-            nums[i][1]=nums[i-1][1];
-            nums[i][2]=nums[i-1][2];
-            nums[i-1][1]=temp;
-            nums[i-1][2]=temp2;
-        }
-    }*/
-    set<pair<int,int>> gua;
-    gua.insert({2e9,0});
-    for(int i=0;i<n;i++){
-        int b=nums[i].second;
-        int c=-nums[i].first.second;
-        auto q=gua.lower_bound({c,-2e9});
-        res[b]=-((*q).second)+1;
-        if((*q).first==c){
-            gua.erase(q);
-        }
-        gua.insert({c,-res[b]});
-        auto p=gua.lower_bound({c,res[b]});
-        while(p!=gua.begin()){
-            p--;
-            if((*p).first<c && -(*p).second<=res[b]){
-                gua.erase(p);
-            }
-        }
-    }
-    /*vector<int> cant(n+5,-1);
-    cant[0]=nums[0][1];
+    vector<int> cant(n,-1);
+    cant[0]=-nums[0].first.second;
     for(int i=1;i<n;i++){
         int limi=0,limd=n-1;
         while(limi!=limd){
             int mid=(limi+limd)/2;
-            if(nums[i][1]<=cant[mid]){
+            if(-nums[i].first.second<=cant[mid]){
                 limi=mid+1;
             }else{
                 limd=mid;
             }
         }
-        while(limi>0){
-            if(nums[i][1]>cant[limi]){
-                limi--;
-            }else{
-                break;
-            }
-        }
-        while(limi<n-1){
-            if(nums[i][1]<=cant[limi]){
-                limi++;
-            }else{
-                break;
-            }
-        }
-        cant[limi]=nums[i][1];
-        res[nums[i][2]]=limi;
-    }*/
-    for(auto u:res){
-        printf("%d ",u-1);
+        cant[limi]=-nums[i].first.second;
+        res[nums[i].second]=limi;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",res[i]);
     }
 }
