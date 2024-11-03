c408e0afeb5b6c100ee9a184d30c9c4751834088
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-11-03 15:47:46 -0400

    a

---
+++ Semana del 31 de octubre al 7 de noviembre/Triangles.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+int n,q,t,a,b;
+int main(){
+    scanf("%d%d",&n,&q);
+    vi nums(n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&nums[i]);
+    }
+    while(q--){
+        scanf("%d%d%d",&t,&a,&b);
+        if(t){
+            //query
+        }else{
+            //update
+        }
+    }
+}
