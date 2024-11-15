474811a9e198438be1b62bfcb3c49a8f43b0aa35
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-11-14 22:04:03 -0400

    a

--- Nose.cpp
+++ Nose.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<long long> fib(21);
+vector<long long> fib(50);
 int main(){
+    fib[1]=fib[2]=1;
+    for(int i=3;i<50;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+        if(fib[i]>=5e8){
+            cout << i;
+            break;
+        }
+    }
 }
--- Semana del 31 de octubre al 7 de noviembre/Triangles.cpp
+++ Semana del 31 de octubre al 7 de noviembre/Triangles.cpp
@@ -1,310 +1,144 @@
 #include <bits/stdc++.h>
-#define LSOne(S) ((S) & -(S))
 using namespace std;
 typedef long long ll;
 typedef vector<ll> vll;
 typedef vector<int> vi;
 int n,q,t,a,b;
 vi nums;
-map<int,int> operator+ (map<int,int> x,map<int,int> y){
-    for(auto u:y){
-        x[u.first]+=u.second;
-    }
-    return x;
-}
-map<int,int> operator- (map<int,int> x,map<int,int> y){
-    for(auto u:y){
-        x[u.first]-=u.second;
-        if(x[u.first]==0){
-            x.erase(u.first);
+vector<vi> st;
+// Que grande Mati
+void build(int pos,int l,int r){
+    if(l==r){
+        st[pos].push_back(nums[l]);
+    }else{
+        int mid=(l+r)>>1;
+        int hiji=(pos<<1)+1,hijd=(pos<<1)+2;
+        build(hiji,l,mid);
+        build(hijd,mid+1,r);
+        int iz=0,de=0,con=1;
+        while(iz<st[hiji].size() && de<st[hijd].size() && con<=44){
+            if(st[hiji][iz]>=st[hijd][de]){
+                st[pos].push_back(st[hiji][iz]);
+                iz++;
+            }else{
+                st[pos].push_back(st[hijd][de]);
+                de++;
+            }
+            con++;
+        }
+        while(iz<st[hiji].size() && con<=44){
+            st[pos].push_back(st[hiji][iz]);
+            iz++;
+            con++;
+        }
+        while(de<st[hijd].size() && con<=44){
+            st[pos].push_back(st[hijd][de]);
+            de++;
+            con++;
         }
     }
-    return x;
 }
-vi operator- (vi x,vi y){
-    for(int i=0;i<32;i++){
-        x[i]-=y[i];
+vi query(int pos,int i,int j,int l,int r){
+    vi res;
+    if(r<i || j<l){
+        return res;
     }
-    return x;
-}
-vi operator+ (vi x,vi y){
-    for(int i=0;i<32;i++){
-        x[i]+=y[i];
+    if(i<=l && r<=j){
+        return st[pos];
     }
-    return x;
+    int mid=(l+r)>>1;
+    int hiji=(pos<<1)+1,hijd=(pos<<1)+2;
+    vi izqui=query(hiji,i,j,l,mid);
+    vi derec=query(hijd,i,j,mid+1,r);
+    int iz=0,de=0,con=1;
+    while(iz<izqui.size() && de<derec.size() && con<=44){
+        if(izqui[iz]>=derec[de]){
+            res.push_back(izqui[iz]);
+            iz++;
+        }else{
+            res.push_back(derec[de]);
+            de++;
+        }
+        con++;
+    }
+    while(iz<izqui.size() && con<=44){
+        res.push_back(izqui[iz]);
+        iz++;
+        con++;
+    }
+    while(de<derec.size() && con<=44){
+        res.push_back(derec[de]);
+        de++;
+        con++;
+    }
+    return res;
 }
-class FenwickTree {
-    private:
-        vector<map<int,int>> ft;
-    public:
-        FenwickTree(vi aaaaa) {
-            int m=aaaaa.size();
-            vector<map<int,int>> acum(m);
-            for(int i=1;i<m;i++){
-                acum[i]=acum[i-1];
-                acum[i][nums[i]]++;
+void update(int pos,int l,int r,int index,int val){
+    if(l==r && l==index){
+        st[pos].pop_back();
+        st[pos].push_back(val);
+        return;
+    }else if(l<=index && index<=r){
+        int mid=(l+r)>>1;
+        int hiji=(pos<<1)+1,hijd=(pos<<1)+2;
+        update(hiji,l,mid,index,val);
+        update(hijd,mid+1,r,index,val);
+        st[pos].clear();
+        int iz=0,de=0,con=1;
+        while(iz<st[hiji].size() && de<st[hijd].size() && con<=44){
+            if(st[hiji][iz]>=st[hijd][de]){
+                st[pos].push_back(st[hiji][iz]);
+                iz++;
+            }else{
+                st[pos].push_back(st[hijd][de]);
+                de++;
             }
-            ft.resize(m);
-            for(int i=1;i<m;i++){
-                ft[i]=acum[i]-acum[i-LSOne(i)];
-            }
+            con++;
         }
-        map<int,int> rsq(int j) {
-            map<int,int> sum;
-            for (; j; j-=LSOne(j)){
-                sum=sum+ft[j];
-            }
-            return sum;
+        while(iz<st[hiji].size() && con<=44){
+            st[pos].push_back(st[hiji][iz]);
+            iz++;
+            con++;
         }
-        map<int,int> rsq(int i, int j) { return rsq(j) - rsq(i-1); }
-        void update(int pos,int val,int ante) {
-            for (; pos < (int)ft.size(); pos += LSOne(pos)){
-                ft[pos][ante]--;
-                if(ft[pos][ante]==0){
-                    ft[pos].erase(ante);
-                }
-                ft[pos][val]++;
-            }
+        while(de<st[hijd].size() && con<=44){
+            st[pos].push_back(st[hijd][de]);
+            de++;
+            con++;
         }
-};
-class PrefixSum {
-    private:
-        vector<map<int,int>> ps;
-    public:
-        PrefixSum(vi aaaaa) {
-            int m=aaaaa.size();
-            ps.resize(m);
-            for(int i=1;i<m;i++){
-                ps[i]=ps[i-1];
-                ps[i][nums[i]]++;
-            }
-        }
-        map<int,int> rsq(int i, int j){return ps[j]-ps[i-1];}
-};
-class TASK4 {
-    private:
-        vector<vi> pot;
-    public:
-        TASK4(vi aaaaa) {
-            int m=aaaaa.size();
-            vector<vi> acum(m,vi(32));
-            for(int i=1;i<m;i++){
-                acum[i]=acum[i-1];
-                acum[i][nums[i]]++;
-            }
-            pot.resize(m);
-            for(int i=1;i<m;i++){
-                pot[i]=acum[i]-acum[i-LSOne(i)];
-            }
-        }
-        vi rsq(int j) {
-            vi sum(32);
-            for (; j; j-=LSOne(j)){
-                sum=sum+pot[j];
-            }
-            return sum;
-        }
-        vi rsq(int i, int j) { return rsq(j) - rsq(i-1); }
-        void update(int pos,int val,int ante) {
-            for (; pos < (int)pot.size(); pos += LSOne(pos)){
-                pot[pos][ante]--;
-                pot[pos][val]++;
-            }
-        }
-};
+    }
+}
 int main(){
     scanf("%d%d",&n,&q);
-    nums.resize(n+1);
-    bool task4=true;
-    bool task5=true;
-    for(int i=1;i<=n;i++){
+    nums.resize(n);
+    st.resize(4*n+5);
+    for(int i=0;i<n;i++){
         scanf("%d",&nums[i]);
-        if(__builtin_popcount(nums[i])!=1){
-            task4=false;
-        }
     }
-    if(task4){
-        for(int i=1;i<=n;i++){
-            nums[i]=__builtin_ctz(nums[i]);
-        }
-        TASK4 aux(nums);
-        while(q--){
-            scanf("%d%d%d",&t,&a,&b);
-            a++;
-            if(t){
-                bool xdround=false;
-                b++;
-                vi prueb=aux.rsq(a,b);
-                int res=0;
-                int temp=0;
-                for(int i=31;i>=0;i--){
-                    if(xdround && prueb[i]>0){
-                        temp+=1<<i;
-                        res=temp;
-                        break;
-                    }
-                    if(prueb[i]>=3){
-                        res=3*(1<<i);
-                        break;
-                    }
-                    if(prueb[i]==2){
-                        temp+=2*(1<<i);
-                        xdround=true;
-                    }
-                }
-                printf("%d\n",res);
+    build(0,0,n-1);
+    while(q--){
+        scanf("%d%d%d",&t,&a,&b);
+        if(t){
+            bool xd=false;
+            vi res=query(0,a,b,0,n-1);
+            if(res.size()<3){
+                // nada
             }else{
-                b=__builtin_ctz(b);
-                int x=nums[a];
-                nums[a]=b;
-                aux.update(a,b,x);
-            }
-        }
-    }else{
-        vector<vi> quer(q,vi(3));
-        for(int qq=0;qq<q;qq++){
-            scanf("%d%d%d",&quer[qq][0],&quer[qq][1],&quer[qq][2]);
-            if(!quer[qq][0]){
-                task5=false;
-            }
-        }
-        if(task5){
-            PrefixSum aux(nums);
-            for(int qq=0;qq<q;qq++){
-                t=quer[qq][0];
-                a=quer[qq][1];
-                b=quer[qq][2];
-                a++;b++;
-                map<int,int> prueb=aux.rsq(a,b);
-                int res=-1;
-                int nue=-1;
-                /*map<int,int> prueb;
-                for(auto u:prueba){
-                    prueb[u.first]=u.second;
-                }*/
-                auto p=prueb.end();
-                p--;
-                int last=1e18,penu=1e17,antepenu=p->first;
-                if(p->second>=2){
-                    last=penu;
-                    penu=antepenu;
-                }
-                if(antepenu+penu>last){
-                    res=antepenu+penu+last;
-                }
-                if(p->second>=3){
-                    last=penu;
-                    penu=antepenu;
-                }
-                if(antepenu+penu>last){
-                    res=antepenu+penu+last;
-                }
-                if(res==-1){
-                    while(p!=prueb.begin()){
-                        p--;
-                        last=penu;
-                        penu=antepenu;
-                        antepenu=p->first;
-                        if(antepenu+penu>last){
-                            res=antepenu+penu+last;
-                            break;
-                        }
-                        if(p->second>=2){
-                            last=penu;
-                            penu=antepenu;
-                        }
-                        if(antepenu+penu>last){
-                            res=antepenu+penu+last;
-                            break;
-                        }
-                        if(p->second>=3){
-                            last=penu;
-                            penu=antepenu;
-                        }
-                        if(antepenu+penu>last){
-                            res=antepenu+penu+last;
-                            break;
-                        }
+                int now=0;
+                while(now<=res.size()-3){
+                    if(res[now]<res[now+1]+res[now+2]){
+                        printf("%d\n",res[now]+res[now+1]+res[now+2]);
+                        xd=true;
+                        break;
                     }
+                    now++;
                 }
-                if(res==-1){
-                    res=0;
+                if(xd){
+                    continue;
                 }
-                printf("%d\n",res);
             }
+            printf("0\n");
         }else{
-            FenwickTree aux(nums);
-            for(int qq=0;qq<q;qq++){
-                t=quer[qq][0];
-                a=quer[qq][1];
-                b=quer[qq][2];
-                a++;
-                if(t){
-                    //query
-                    b++;
-                    map<int,int> prueb=aux.rsq(a,b);
-                    int res=-1;
-                    int nue=-1;
-                    /*map<int,int> prueb;
-                    for(auto u:prueba){
-                        prueb[u.first]=u.second;
-                    }*/
-                    auto p=prueb.end();
-                    p--;
-                    int last=1e18,penu=1e17,antepenu=p->first;
-                    if(p->second>=2){
-                        last=penu;
-                        penu=antepenu;
-                    }
-                    if(antepenu+penu>last){
-                        res=antepenu+penu+last;
-                    }
-                    if(p->second>=3){
-                        last=penu;
-                        penu=antepenu;
-                    }
-                    if(antepenu+penu>last){
-                        res=antepenu+penu+last;
-                    }
-                    if(res==-1){
-                        while(p!=prueb.begin()){
-                            p--;
-                            last=penu;
-                            penu=antepenu;
-                            antepenu=p->first;
-                            if(antepenu+penu>last){
-                                res=antepenu+penu+last;
-                                break;
-                            }
-                            if(p->second>=2){
-                                last=penu;
-                                penu=antepenu;
-                            }
-                            if(antepenu+penu>last){
-                                res=antepenu+penu+last;
-                                break;
-                            }
-                            if(p->second>=3){
-                                last=penu;
-                                penu=antepenu;
-                            }
-                            if(antepenu+penu>last){
-                                res=antepenu+penu+last;
-                                break;
-                            }
-                        }
-                    }
-                    if(res==-1){
-                        res=0;
-                    }
-                    printf("%d\n",res);
-                }else{
-                    //update
-                    int x=nums[a];
-                    nums[a]=b;
-                    aux.update(a,b,x);
-                }
-            }
+            update(0,0,n-1,a,b);
         }
     }
 }
