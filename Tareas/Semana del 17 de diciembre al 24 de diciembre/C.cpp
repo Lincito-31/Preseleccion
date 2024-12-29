73eb039b087703369fc37f391334755f8dbba97e
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-12-29 17:22:47 -0400

    a

--- Tareas/Semana del 17 de diciembre al 24 de diciembre/C.cpp
+++ Tareas/Semana del 17 de diciembre al 24 de diciembre/C.cpp
@@ -4,45 +4,89 @@ typedef long long ll;
 typedef pair<ll,ll> pll;
 typedef vector<pll> vpll;
 typedef vector<ll> vll;
-ll t,n,m,a,b,c;
+int n,con=1;
+char a,b,c;
+map<string,int> clav;
+map<int,string> revclav;
 int main(){
-    scanf("%lld",&t);
-    while(t--){
-        scanf("%lld%lld",&n,&m);
-        vector<vpll> graph(n);
-        vll slowness(n);
-        while(m--){
-            scanf("%lld%lld%lld",&a,&b,&c);
-            a--;b--;
-            graph[a].push_back({b,c});
-            graph[b].push_back({a,c});
+    scanf("%d",&n);
+    vector<int> graph[2*n+5];
+    vector<int> in(2*n+5);
+    vector<int> out(2*n+5);
+    for(int i=0;i<n;i++){
+        scanf(" %c%c%c",&a,&b,&c);
+        string from="",to="";
+        from+=a;from+=b;
+        to+=b;to+=c;
+        if(clav[from]==0){
+            clav[from]=con;
+            revclav[con]=from;
+            con++;
         }
-        for(int i=0;i<n;i++){
-            scanf("%lld",&slowness[i]);
+        if(clav[to]==0){
+            clav[to]=con;
+            revclav[con]=to;
+            con++;
         }
-        vector<vll> time(n,vll(n,1e18));
-        time[0][0]=0;
-        priority_queue<vll> dijk;
-        dijk.push({0,0,0});
-        while(!dijk.empty()){
-            vll top=dijk.top();
-            dijk.pop();
-            ll tiempo=-top[0],nodo=top[1],bici=top[2];
-            for(auto u:graph[nodo]){
-                if(tiempo+u.second*slowness[bici]<time[u.first][bici]){
-                    time[u.first][bici]=tiempo+u.second*slowness[bici];
-                    if(slowness[bici]<=slowness[u.first]){
-                        dijk.push({-time[u.first][bici],u.first,bici});
-                    }else{
-                        dijk.push({-time[u.first][bici],u.first,u.first});
-                    }
-                }
+        graph[clav[from]].push_back(clav[to]);
+        out[clav[from]]++;
+        in[clav[to]]++;
+    }
+    int ini=0,end=0;
+    for(int i=1;i<=2*n;i++){
+        if(in[i]==out[i]){
+            //nada
+        }else if(in[i]==out[i]+1){
+            if(end){
+                printf("NO");
+                return 0;
             }
+            end=i;
+        }else if(in[i]+1==out[i]){
+            if(ini){
+                printf("NO");
+                return 0;
+            }
+            ini=i;
+        }else{
+            printf("NO");
+            return 0;
         }
-        ll mini=1e18;
-        for(auto u:time.back()){
-            mini=min(u,mini);
+    }
+    if((ini==0&&end!=0)||(ini!=0&&end==0)){
+        printf("NO");
+        return 0;
+    }else if(!(ini&&end)){
+        ini=1;
+    }
+    int now=ini;
+    stack<int> cola;
+    stack<int> caminoE;
+    cola.push(now);
+    while(!cola.empty()){
+        if(graph[now].empty()){
+            caminoE.push(cola.top());
+            cola.pop();
+            if(cola.empty()){
+                break;
+            }
+            now=cola.top();
+        }else{
+            int next=graph[now].back();
+            graph[now].pop_back();
+            now=next;
+            cola.push(next);
         }
-        printf("%lld\n",mini);
+    }
+    if(caminoE.size()<n){
+        printf("NO");
+        return 0;
+    }
+    printf("YES\n");
+    int xx=caminoE.top();caminoE.pop();
+    printf("%c%c",revclav[xx][0],revclav[xx][1]);
+    while(!caminoE.empty()){
+        printf("%c",revclav[caminoE.top()][1]);
+        caminoE.pop();
     }
 }
