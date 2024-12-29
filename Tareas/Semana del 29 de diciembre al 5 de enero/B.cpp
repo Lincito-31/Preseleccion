596d2ab457df5ddb962e26590d1e3ab4056e658d
LIN MING OU XU <linmingouxu0331@gmail.com>
2024-12-29 15:01:28 -0400

    a

--- Tareas/Semana del 29 de diciembre al 5 de enero/B.cpp
+++ Tareas/Semana del 29 de diciembre al 5 de enero/B.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 typedef long long ll;
 ll n;
+ll calc(ll x){
+    ll sum=0;
+    while(x>0){
+        sum+=x%10;
+        x/=10;
+    }
+    return sum;
+}
 int main(){
     scanf("%lld",&n);
-    for(int i=0;in)
+    while(n%2==0){
+        n/=2;
+    }
+    while(n%5==0){
+        n/=5;
+    }
+    ll mini=1e9;
+    for(int i=1;i<=3e7;i++){
+        mini=min(mini,calc(n*i));
+    }
+    printf("%lld",mini);
 }
