248b1ea3c08a59edbff8e97d9c9785d288ad3fc0
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-01-05 17:49:16 -0400

    a

---
+++ codeforces/Codeforces Round 703 (Div. 2)/D.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,k;
+int main(){
+    scanf("%d%d",&n,&k);
+    for(int i=0;i<n;i++){
+
+    }
+}
--- codeforces/Codeforces Round 919 (Div. 2)/C.cpp
+++ codeforces/Codeforces Round 919 (Div. 2)/C.cpp
@@ -4,14 +4,40 @@ int t,n;
 int main(){
     scanf("%d",&t);
     while(t--){
+        int con=0;
         scanf("%d",&n);
-        // caso if k=n;
-        for(int i=2;i*i<n;i++){
-            if(n%i==0){
-                // check for i;
-                
+        vector<int> nums(n);
+        for(int i=0;i<n;i++){
+            scanf("%d",&nums[i]);
+        }
+        int mcd=0;
+        for(int i=1;i*i<=n;i++){
+            int x=i;
+            if(n%x==0){
+                mcd=0;
+                for(int j=1;j<n/x;j++){
+                    for(int k=0;k<x;k++){
+                        mcd=gcd(mcd,nums[j*x+k]-nums[(j-1)*x+k]);
+                    }
+                }
+                if(mcd!=1){
+                    con++;
+                }
+                mcd=0;
+                if(x==n/x){
+                    continue;
+                }
+                x=n/x;
+                for(int j=1;j<n/x;j++){
+                    for(int k=0;k<x;k++){
+                        mcd=gcd(mcd,nums[j*x+k]-nums[(j-1)*x+k]);
+                    }
+                }
+                if(mcd!=1){
+                    con++;
+                }
             }
         }
-        // caso if k*k=n;
+        printf("%d\n",con);
     }
 }
