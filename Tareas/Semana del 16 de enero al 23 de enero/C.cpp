17cb8bb4715cccaff632668d01912f13e6804dd1
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-01-17 21:52:19 -0400

    a

--- Tareas/Semana del 16 de enero al 23 de enero/C.cpp
+++ Tareas/Semana del 16 de enero al 23 de enero/C.cpp
@@ -3,6 +3,24 @@ using namespace std;
 int n;
 vector<int> nums;
 vector<vector<int>> dp;
+int solve(int x,int y){
+    if(x>=y){
+        return 1;
+    }
+    if(dp[x][y]!=1e9){
+        return dp[x][y];
+    }
+    dp[x][y]=1+solve(x+1,y);
+    if(nums[x]==nums[y]){
+        dp[x][y]=min(dp[x][y],solve(x+1,y-1));
+    }
+    for(int i=x+1;i<y;i++){
+        if(nums[i]==nums[x]){
+            dp[x][y]=min(solve(x+1,i-1)+solve(i+1,y),dp[x][y]);
+        }
+    }
+    return dp[x][y];
+}
 int main(){
     scanf("%d",&n);
     nums.resize(n);
@@ -10,5 +28,5 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&nums[i]);
     }
-    solve()
+    printf("%d",solve(0,n-1));
 }
