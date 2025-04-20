a77b295f6aa248ce3ebd1362412bc370b86c7ab9
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-20 11:06:31 -0400

    a

--- Kattis/Baby Bites.cpp
+++ Kattis/Baby Bites.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int t,n;
-int arr[1000];
+string st;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     cin >> t;
-    while(t--){
-        cin >> n;
-        for(int i=0;i<n;i++){
-            cin >> arr[i];
-        }
-        for(int i=1;i<n-1;i++){
-            if(arr[i-1]<arr[i] && arr[i]<arr[i+1]){
-                continue;
+    for(int i=1;i<=t;i++){
+        cin >> st;
+        if(st!="mumble"){
+            n=stoi(st);
+            if(n!=i){
+                cout << "something is fishy";
+                return 0;
             }
-            if(arr[i-1]<arr[i+1]){
-                cout << i+1 << '\n';
-            }else{
-                cout << i+2 << '\n';
-            }
-            break;
         }
     }
+    cout << "makes sense";
 }
