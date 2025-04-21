68c5b4b68878d7948250ba966a05113b6f3849fb
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-21 17:04:54 -0400

    a

--- Kattis/Climbing Stairs.cpp
+++ Kattis/Climbing Stairs.cpp
@@ -2,10 +2,10 @@
 #define ALL(x) x.begin(),x.end()
 using namespace std;
 typedef long long ll;
-int w,l,n,a;
-char t;
+int a,b,c;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    
+    cin >> a >> b >> c;
+    cout << a+b+c-min({a,b,c});
 }
