2e6885a4caf45e5a97d5689d0849d2d17ad20d75
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-23 17:12:32 -0400

    a

--- Kattis/Exactly Electrical.cpp
+++ Kattis/Exactly Electrical.cpp
@@ -2,23 +2,14 @@
 #define ALL(x) x.begin(),x.end()
 using namespace std;
 typedef long long ll;
-int N,B,H,W,P,x,tot=1e9;;
+int a,b,c,d,e;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    cin >> N >> B >> H >> W;
-    while(H--){
-        cin >> P;
-        for(int i=0;i<W;i++){
-            cin >> x;
-            if(x>=N){
-                tot=min(tot,N*P);
-            }
-        }
-    }
-    if(tot<=B){
-        cout << tot;
+    cin >> a >> b >> c >> d >> e;
+    if(((e-(abs(a-c)+abs(b-d)))>=0)&&((e-abs(a-c)+abs(b-d))%2==0)){
+        cout << "Y";
     }else{
-        cout << "stay home";
+        cout << 'N';
     }
 }
