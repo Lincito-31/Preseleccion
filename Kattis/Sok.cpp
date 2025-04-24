27531113ebe72169bb8827d58f4de2ed6d153b32
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-23 21:14:50 -0400

    a

--- Kattis/Sok.cpp
+++ Kattis/Sok.cpp
@@ -2,35 +2,16 @@
 #define ALL(x) x.begin(),x.end()
 using namespace std;
 typedef long long ll;
-int a,b,c,x;
+double a,b,c,d,e,f;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    while (cin >> a && a){
-        bool xd=true;
-        cin >> b;
-        vector<bool> exi(10000,false);
-        while(a--){
-            cin >> x;
-            exi[x]=true;
-        }
-        while(b--){
-            cin >> a >> c;
-            int con=0;
-            while(a--){
-                cin >> x;
-                if(exi[x]){
-                    con++;
-                }
-            }
-            if(con<c){
-                xd=false;
-            }
-        }
-        if(xd){
-            cout << "yes\n";
-        }else{
-            cout << "no\n";
-        }
+    cin >> a >> b >> c >> d >> e >> f;
+    if(a*e<=b*d && a*f<=d*c){
+        cout << fixed << setprecision(6) << a-d*a/d << " " << b-e*a/d << " " << c-f*a/d;
+    }else if(b*d<=e*a && b*f<=e*c){
+        cout << fixed << setprecision(6) << a-d*b/e << " " << b-e*b/e << " " << c-f*b/e;
+    }else{
+        cout << fixed << setprecision(6) << a-d*c/f << " " << b-e*c/f << " " << c-f*c/f;
     }
 }
