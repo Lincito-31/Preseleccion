b3182abe3a98cd32447207a481a3407c4cb38d59
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-26 15:47:17 -0400

    a

---
+++ Kattis/Beekeeper.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+using namespace std;
+typedef long long ll;
+int t,con,maxi;
+string x,res;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    while(cin >> t){
+        res="";
+        maxi=0;
+        while(t--){
+            con=0;
+            cin >> x;
+            for(int i=1;i<x.size();i++){
+                if(x[i]==x[i-1] && (x[i]=='a' || x[i]=='o' || x[i]=='e' || x[i]=='i' || x[i]=='u' || x[i]=='y')){
+                    con++;
+                }
+            }
+            if(maxi<con){
+                maxi=con;
+                res=x;
+            }
+        }
+        cout << res << '\n';
+    }
+}
