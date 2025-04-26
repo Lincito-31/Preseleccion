00b0c795109375074563cfdc93999e7ab5d3d3bf
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-26 15:42:38 -0400

    a

--- Kattis/Another Brick in the Wall.cpp
+++ Kattis/Another Brick in the Wall.cpp
@@ -2,37 +2,27 @@
 #define ALL(x) x.begin(),x.end()
 using namespace std;
 typedef long long ll;
-int t,n,m,x;
-int need[10][30];
-int price[10],canti[10],con[30];
+int h,w,b,x,n,m;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-    cin >> t;
-    while(t--){
-        cin >> n >> m;
-        memset(canti,0,sizeof canti);
-        memset(con,0,sizeof con);
-        for(int i=0;i<n;i++){
-            cin >> canti[i];
-            for(int j=0;j<canti[i];j++){
-                cin >> need[i][j];
-                need[i][j]--;
-            }
-            cin >> price[i];
+    cin >> h >> w >> b;
+    while(b--){
+        cin >> x;
+        if(x+m<=w){
+            m+=x;
+        }else{
+            cout << "NO";
+            return 0;
         }
-        for(int i=0;i<m;i++){
-            cin >> x;
-            con[i]+=x;
+        if(m==w){
+            m=0;
+            n++;
         }
-        ll sum=0;
-        for(int i=0;i<n;i++){
-            int now=1e9;
-            for(int j=0;j<canti[i];j++){
-                now=min(now,con[need[i][j]]);
-            }
-            sum+=now*price[i];
+        if(n==h){
+            cout << "YES";
+            return 0;
         }
-        cout << sum << '\n';
     }
+    cout << "NO";
 }
