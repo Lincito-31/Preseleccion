83971b47011a09eda23173c67d066ed5eed1b0fc
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-26 17:38:30 -0400

    a

---
+++ Kattis/Howl.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+using namespace std;
+typedef long long ll;
+string st;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cin >> st;
+    cout << st << 'O';
+}
---
+++ Kattis/Shattered Cake.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+using namespace std;
+typedef long long ll;
+ll W,s,t,a,b;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cin >> W >> t;
+    while(t--){
+        cin >> a >> b;
+        s+=a*b;
+    }
+    cout << s/W;
+}
