2ddde7bca963d227b8b7ed456cd67a54b1c38091
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-04-27 14:48:03 -0400

    a

--- Uva Online Judge/11786.cpp
+++ Uva Online Judge/11786.cpp
@@ -2,20 +2,38 @@
 #define ALL(x) x.begin(),x.end()
 using namespace std;
 typedef long long ll;
-int t,con;
-double l,w,h,k;
+int t,altnow,sum;
+vector<int> alt(20001,1e9);
+string s;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
     cin >> t;
     while(t--){
-        cin >> l >> w >> h >> k;
-        if(((l<=56 && w<=45 && h<=25)||((l+w+h)<=125))&& k<=7){
-            cout << 1 << '\n';
-            con++;
-        }else{
-            cout << 0 << '\n';
+        sum=0;
+        altnow=10000;
+        alt.assign(20001,1e9);
+        cin >> s;
+        if(s[0]=='\\'){
+            alt[altnow]=0;
         }
+        for(int i=1;i<s.size();i++){
+            if(s[i]=='/'){
+                if(s[i-1]=='/'){
+                    altnow++;
+                }
+                sum+=max(0,i-alt[altnow]);
+            }else if(s[i]=='\\'){
+                if(s[i-1]!='/'){
+                    altnow--;
+                }
+                alt[altnow]=i;
+            }else{
+                if(s[i-1]=='/'){
+                    altnow++;
+                }
+            }
+        }
+        cout << sum << '\n';
     }
-    cout << con << '\n';
 }
