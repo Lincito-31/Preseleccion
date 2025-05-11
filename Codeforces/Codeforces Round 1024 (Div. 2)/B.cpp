50deca85157bfe34ff349e51082471e5aba5296e
LIN MING OU XU <linmingouxu0331@gmail.com>
2025-05-11 10:28:51 -0400

    a

---
+++ Codeforces/Codeforces Round 1024 (Div. 2)/B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> intset;
+int t,n;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cin >> t;
+    while(t--){
+        cin >> n;
+    }
+}
